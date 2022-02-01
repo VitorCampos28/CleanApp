f3d6ab882f3e758eb4853af84eeae43b42024172
Vitor <vitorcampos@brq.com>
2022-01-31 23:01:31 -0300

    add AddAccount use case

---
+++ Domain/AddAcount.swift
@@ -0,0 +1,22 @@
+//
+//  AddAcount.swift
+//  Domain
+//
+//  Created by Vitor Natal de Oliveira Campos on 31/01/22.
+//
+
+import Foundation
+
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void )
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
+
+
