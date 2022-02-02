e788c7a2d85f513da4ea67e62036ef7c4550263a
Vitor <vitorcampos@brq.com>
2022-02-02 17:12:57 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Vitor Natal de Oliveira Campos on 02/02/22.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected 
+}
