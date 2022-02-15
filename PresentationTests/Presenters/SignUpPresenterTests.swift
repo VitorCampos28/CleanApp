5dc327748ebf83d86e9fcd19e2d8a70b01b4d334
Vitor <vitorcampos@brq.com>
2022-02-15 12:50:23 -0300

    ensure SignUpPresenter shows error message when addAccount completes with error

--- PresentationTests/Presenters/SignUpPresenterTests.swift
+++ PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -50,6 +50,14 @@ class SignUpPresenterTests: XCTestCase {
         sut.signUp(viewModel: makeSignUpViewModel())
         XCTAssertEqual(alertViewSpy.viewModel, makeInvalidAlertViewModel(fieldName: "Email"))
     }
+    func test_signUp_should_show_error_message_if_addAccount_fails() throws {
+        let alertViewSpy = AlertViewSpy()
+        let addAccountSpy = AddAccountSpy()
+        let sut = makeSut(alertView: alertViewSpy, addAccount: addAccountSpy)
+        sut.signUp(viewModel: makeSignUpViewModel())
+        addAccountSpy.completeWithError(.unexpected)
+        XCTAssertEqual(alertViewSpy.viewModel, makeErrorAlertViewModel(message: "Algo inesperado aconteceu tente novamente em algums instantes"))
+    }
     func test_signUp_should_call_email_validator_with_corret_email() throws {
         let emailValidatorSpy = EmailValidatorSpy()
         let sut = makeSut(emailValidator: emailValidatorSpy)
@@ -81,6 +89,9 @@ extension SignUpPresenterTests {
     func makeInvalidAlertViewModel(fieldName: String) -> AlertViewModel {
         return  AlertViewModel(title: "Falha na validação", message: "\(fieldName) é invalido")
     }
+    func makeErrorAlertViewModel(message: String) -> AlertViewModel {
+        return  AlertViewModel(title: "Error", message: message)
+    }
     
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
@@ -104,9 +115,14 @@ extension SignUpPresenterTests {
     
     class AddAccountSpy: AddAccount {
         var addAccountModel: AddAccountModel?
+        var completion: ((Result<AccountModel, DomainError>) -> Void)?
         
         func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
             self.addAccountModel = addAccountModel
+            self.completion = completion
+        }
+        func completeWithError(_ error: DomainError) {
+            completion?(.failure(error))
         }
     }
 }
