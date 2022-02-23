ce71fc4b46a283fc2ddd6baeb65de6a8c9e0e7b9
Vitor <vitorcampos@brq.com>
2022-02-23 18:35:24 -0300

    move signUpViewModel to addAccountModel to a Mapper

---
+++ Presentation/Presenters/SignUp/SignUpMapper.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpMapper.swift
+//  Presentation
+//
+//  Created by Vitor Natal de Oliveira Campos on 23/02/22.
+//
+
+import Foundation
+import Domain
+
+public final class SignUpMapper {
+    static func toAddAcountModel(viewModel: SignUpViewModel) -> AddAccountModel {
+        return AddAccountModel(name: viewModel.name!, email: viewModel.email!, password: viewModel.password!, passwordConfirmation: viewModel.passwordConfirmation!)
+        loadingView.display(viewModel: LoadingViewModel(isLoading: true))
+    }
+}
