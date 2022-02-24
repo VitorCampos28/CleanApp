31f5ac83402dee6893350569c4e06ee030845038
Vitor <vitorcampos@brq.com>
2022-02-24 15:26:58 -0300

    allow all targets to run on both IOS and macOS

---
+++ IOSUITests/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IOSUITests.swift
+//  IOSUITests
+//
+//  Created by Vitor Natal de Oliveira Campos on 24/02/22.
+//
+
+import XCTest
+
+class IOSUITests: XCTestCase {
+     func test_() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
