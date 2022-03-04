a9b0e83750868ac48a59cfb0d967a65d47773666
Vitor <vitorcampos@brq.com>
2022-03-04 18:59:29 -0300

    add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Vitor Natal de Oliveira Campos on 04/03/22.
+//
+
+import XCTest
+
+class ValidationTests: XCTestCase {
+    
+    func tests_()  {
+        
+    }
+    
+}
