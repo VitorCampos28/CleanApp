9685fe7250618105528e5329b470a4a1777ee838
Vitor <vitorcampos@brq.com>
2022-03-04 19:20:09 -0300

    move EmailValidatorAdapter  to its own file

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,19 @@
+//
+//  EmailValidatorAdapter.swift
+//  ValidationTests
+//
+//  Created by Vitor Natal de Oliveira Campos on 04/03/22.
+//
+
+import Foundation
+import Presentation
+
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
