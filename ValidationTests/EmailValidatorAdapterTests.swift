67d360d7ab7e9c756b2004d9612eca5491eab164
Vitor <vitorcampos@brq.com>
2022-03-04 19:15:45 -0300

    move SUT creation to a factory method

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -18,21 +18,27 @@ public final class EmailValidatorAdapter: EmailValidator {
     }
 }
 
-class ValidationTests: XCTestCase {
+class EmailValidatorAdapterTests: XCTestCase {
     
     func tests_invalid_emails()  {
-        let sut = EmailValidatorAdapter()
-        XCTAssertFalse( sut.isValid(email: "rr"))
-        XCTAssertFalse( sut.isValid(email: "rr@"))
-        XCTAssertFalse( sut.isValid(email: "rr@rr"))
-        XCTAssertFalse( sut.isValid(email: "rr@rr."))
-        XCTAssertFalse( sut.isValid(email: "@rr.com"))
+        let sut = makeSut()
+        XCTAssertFalse(sut.isValid(email: "rr"))
+        XCTAssertFalse(sut.isValid(email: "rr@"))
+        XCTAssertFalse(sut.isValid(email: "rr@rr"))
+        XCTAssertFalse(sut.isValid(email: "rr@rr."))
+        XCTAssertFalse(sut.isValid(email: "@rr.com"))
     }
     func tests_valid_emails()  {
-        let sut = EmailValidatorAdapter()
-        XCTAssertTrue( sut.isValid(email: "vitor@gmail.com"))
-        XCTAssertTrue( sut.isValid(email: "vitor@hotmail.com"))
-        XCTAssertTrue( sut.isValid(email: "aiFBQUYDF@adiufgbauyfhi.com.br"))
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "vitor@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "vitor@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "aiFBQUYDF@adiufgbauyfhi.com.br"))
     }
     
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
+    }
 }
