e72de03a21a2915257c19084318b3230adb37a8a
Vitor <vitorcampos@brq.com>
2022-03-04 19:12:06 -0300

    Ensure EmailValidatorAdapter return true if  a valid email is provided

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -6,11 +6,33 @@
 //
 
 import XCTest
+import Presentation
+
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
 
 class ValidationTests: XCTestCase {
     
-    func tests_()  {
-        
+    func tests_invalid_emails()  {
+        let sut = EmailValidatorAdapter()
+        XCTAssertFalse( sut.isValid(email: "rr"))
+        XCTAssertFalse( sut.isValid(email: "rr@"))
+        XCTAssertFalse( sut.isValid(email: "rr@rr"))
+        XCTAssertFalse( sut.isValid(email: "rr@rr."))
+        XCTAssertFalse( sut.isValid(email: "@rr.com"))
+    }
+    func tests_valid_emails()  {
+        let sut = EmailValidatorAdapter()
+        XCTAssertTrue( sut.isValid(email: "vitor@gmail.com"))
+        XCTAssertTrue( sut.isValid(email: "vitor@hotmail.com"))
+        XCTAssertTrue( sut.isValid(email: "aiFBQUYDF@adiufgbauyfhi.com.br"))
     }
     
 }
