26bda15bb6eaf08a36cdf2a23a5084f630993e6c
Vitor <vitorcampos@brq.com>
2022-03-17 16:15:59 -0300

    move WeakVarProxy to its own file

---
+++ Main/Proxies/WeakVarProxy.swift
@@ -0,0 +1,31 @@
+//
+//  WeakVarProxy.swift
+//  Main
+//
+//  Created by Vitor Natal de Oliveira Campos on 17/03/22.
+//
+
+import Foundation
+import Presentation
+
+final class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+    
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
+
