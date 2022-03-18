8d4838bd9948bd8eb0c52d8a5af1c0236698a0bc
Vitor <vitorcampos@brq.com>
2022-03-18 19:47:32 -0300

    move MainQueueDispatchDecorator to its own file

---
+++ Main/Decorators/MainQueueDispatchDecorator.swift
@@ -0,0 +1,29 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  Main
+//
+//  Created by Vitor Natal de Oliveira Campos on 18/03/22.
+//
+
+import Foundation
+import Domain
+
+public final class MainQueueDispatchDecorator<T> {
+    private let instance: T
+    public init(_ instance: T){
+        self.instance = instance
+    }
+    
+    func dispatch(completion: @escaping () -> Void ){
+        guard Thread.isMainThread else { return DispatchQueue.main.async(execute: completion) }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.add(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
