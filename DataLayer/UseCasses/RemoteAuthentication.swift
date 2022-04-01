3c671a0c063f590ccbb879399dd2f04e648374c8
Vitor Campos <vito.r.natal@hotmail.com>
2022-04-01 17:59:38 -0300

    ënsure RemoteAuth completes with unexpected error if HttpClient completes with noConnectivity error

--- DataLayer/UseCasses/RemoteAuthentication.swift
+++ DataLayer/UseCasses/RemoteAuthentication.swift
@@ -15,9 +15,26 @@ public final class RemoteAuthentication {
         self.url = url
         self.httpClient = httpClient
     }
-    public func auth(authenticationModel: AuthenticationModel) {
-        httpClient.post(to: url, with: authenticationModel.toData()) { _ in
-            
+    public func auth(authenticationModel: AuthenticationModel, completion: @escaping (AddAccount.Result) -> Void) {
+        httpClient.post(to: url, with: authenticationModel.toData()) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case .success: break
+            case .failure: completion(.failure(.unexpected))
+//            case .success(let data):
+//                if let model: AccountModel = data?.toModel() {
+//                    completion(.success(model))
+//                } else {
+//                    completion(.failure(.unexpected))
+//                }
+//            case .failure(let error):
+//                switch error {
+//                case .forbidden:
+//                    completion(.failure(.emailInUse))
+//                default:
+//                    completion(.failure(.unexpected))
+//                }
+            }
         }
     }
     
