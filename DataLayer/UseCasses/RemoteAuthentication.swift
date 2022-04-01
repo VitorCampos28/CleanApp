4f980f114cd8f0999625d711c48c6e023beea275
Vitor Campos <vito.r.natal@hotmail.com>
2022-04-01 17:36:38 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL

---
+++ DataLayer/UseCasses/RemoteAuthentication.swift
@@ -0,0 +1,8 @@
+//
+//  RemoteAuthentication.swift
+//  DataLayer
+//
+//  Created by Vitor Campos on 01/04/22.
+//
+
+import Foundation
---
+++ DataLayerTests/UseCasses/RemoteAutheticationTests.swift
@@ -0,0 +1,8 @@
+//
+//  RemoteAutheticationTests.swift
+//  DataLayerTests
+//
+//  Created by Vitor Campos on 01/04/22.
+//
+
+import Foundation
