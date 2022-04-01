b261fbb554f0e3cf1909a4d8a2463bda3cc903bf
Vitor Campos <vito.r.natal@hotmail.com>
2022-04-01 17:38:39 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL

--- DataLayer/UseCasses/RemoteAuthentication.swift
+++ DataLayer/UseCasses/RemoteAuthentication.swift
@@ -4,5 +4,22 @@
 //
 //  Created by Vitor Campos on 01/04/22.
 //
-
 import Foundation
+import Domain
+
+public final class RemoteAuthentication {
+    private let url: URL
+    private let httpClient: HttpPostClient
+    
+    public init (url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    public func auth() {
+        httpClient.post(to: url, with: nil) { _ in
+            
+        }
+    }
+    
+}
+
--- DataLayerTests/UseCasses/RemoteAutheticationTests.swift
+++ DataLayerTests/UseCasses/RemoteAutheticationTests.swift
@@ -5,4 +5,28 @@
 //  Created by Vitor Campos on 01/04/22.
 //
 
-import Foundation
+import XCTest
+import Domain
+import DataLayer
+
+class RemoteAutheticationTests: XCTestCase {
+    
+    func test_add_should_call_httpClient_with_correct_url() {
+        let url = makeURL()
+        let (sut, httpClientSpy) = makeSut(url: url)
+        sut.auth()
+        XCTAssertEqual(httpClientSpy.urls, [url])
+    }
+}
+
+extension RemoteAutheticationTests {
+    
+    func makeSut(url: URL = URL(string: "http://any-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteAuthentication, HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAuthentication(url: url, httpClient: httpClientSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: httpClientSpy, file: file, line: line)
+        return (sut, httpClientSpy)
+    }
+    
+}
