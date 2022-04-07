56b68810a5e35d1f9e964b60b662d0e7d005c2e8
Vitor Campos <vito.r.natal@hotmail.com>
2022-04-07 18:18:31 -0300

    ensure LoginPresenter show and hides loading correctly

--- PresentationTests/Presenters/Login/LoginPresenterTests.swift
+++ PresentationTests/Presenters/Login/LoginPresenterTests.swift
@@ -78,11 +78,31 @@ class LoginPresenterTests: XCTestCase {
         authenticationSpy.completeWithAccount(makeAccountModel())
         wait(for: [exp], timeout: 1)
     }
+    func test_signUp_should_show_loading_before_and_after_addAccount()  {
+        let loadingViewSpy = LoadingViewSpy()
+        let authenticationSpy = AuthenticationSpy()
+        let sut = makeSut(authentication: authenticationSpy, loadingView: loadingViewSpy)
+        let exp = expectation(description: "Waiting...")
+        loadingViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, LoadingViewModel(isLoading: true))
+            exp.fulfill()
+        }
+        sut.login(viewModel: makeLoginViewModel())
+        wait(for: [exp], timeout: 1)
+        
+        let exp2 = expectation(description: "Waiting...")
+        loadingViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, LoadingViewModel(isLoading: false))
+            exp2.fulfill()
+        }
+        authenticationSpy.completeWithError(.unexpected)
+        wait(for: [exp2], timeout: 1)
+    }
 }
 
 extension LoginPresenterTests {
-    func makeSut(alertView: AlertViewSpy = AlertViewSpy(), authentication: AuthenticationSpy = AuthenticationSpy(), validation: ValidationSpy = ValidationSpy() , file: StaticString = #file, line: UInt = #line)-> LoginPresenter {
-        let sut = LoginPresenter(validation: validation, alertView: alertView, authentication: authentication)
+    func makeSut(alertView: AlertViewSpy = AlertViewSpy(), authentication: AuthenticationSpy = AuthenticationSpy(), loadingView: LoadingViewSpy = LoadingViewSpy(), validation: ValidationSpy = ValidationSpy() , file: StaticString = #file, line: UInt = #line)-> LoginPresenter {
+        let sut = LoginPresenter(validation: validation, alertView: alertView, authentication: authentication, loadingView: loadingView)
         checkMemoryLeak(for: sut, file: file, line: line)
         return (sut)
     }
