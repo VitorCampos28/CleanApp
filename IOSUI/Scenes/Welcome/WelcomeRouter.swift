48d76ce1618900cdc28a4390cfac39e29b6a66b2
Vitor Campos <vito.r.natal@hotmail.com>
2022-04-18 15:37:38 -0300

    Move WelcomeRouter to its own file

---
+++ IOSUI/Scenes/Welcome/WelcomeRouter.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeRouter.swift
+//  IOSUI
+//
+//  Created by Vitor Campos on 18/04/22.
+//
+
+import Foundation
+
+public final class WelcomeRouter {
+    private let nav: NavigationController
+    private let loginFactory: () -> LoginViewController
+    private let signUpFactory: () -> SignUpViewController
+    
+    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController){
+        self.nav = nav
+        self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
+    }
+    public func goToLogin(){
+        nav.pushViewController(loginFactory())
+    }
+    public func goToSignUp(){
+        nav.pushViewController(signUpFactory())
+    }
+}
